19f683d134c9a06b4b6b2537f419b9f7e59ff452
heycisco <mail@starchenkov.pro>
2021-08-17 12:00:28 +0300

    wp-upload

--- wp-ebanoe/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside class="sidebar" role="complementary">
-<?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
-<div class="primary" class="widget-area">
-<ul class="sidebar-widget-list">
-<?php dynamic_sidebar( 'primary-widget-area' ); ?>
-</ul>
-</div>
-<?php endif; ?>
-</aside>
